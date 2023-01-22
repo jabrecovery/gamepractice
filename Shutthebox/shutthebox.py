eaf2adbb85b15f23154ffe6446405d447122f041
Jeffrey Bowman <jabrecovery@proton.me>
2023-01-22 13:01:00 -0500

    Some basic code for simple version

--- Shutthebox/shutthebox.py
+++ Shutthebox/shutthebox.py
@@ -1,6 +1,34 @@
+import random
 
-print("Welcomg to Shut the Box!")
-player = input('What is your name?')
+print("Welcome to Shut the Box!")
+
+start_num = [1, 2, 3, 4, 5, 6]
+
+while len(start_num) > 0:
+    num = random.randint(1,6)
+    print(num)
+    print('Choose a number to remove.')
+    user_num = input()
+    start_num.remove(int(user_num))
+    if num not in start_num:
+        print('The game is over!')
+        print(sum(start_num))
+    print(start_num)
+
+
+
+#class Game:
+   # num_1 = random.randint(1,6)
+   # num_2 = random.randint(1,6)
+    #print(num_1, num_2)
+    #begin_num = [1,2,3,4,5,6,7,8,9]
+
+#while begin_num
+#begin_num = [1,2,3,4,5,6,7,8,9]
+#print("Choose a number to remove.")
+#user_selection = input()
+#game_num = begin_num.remove(int(user_selection))
+#print(begin_num)
 
 
 
@@ -10,7 +38,11 @@ player = input('What is your name?')
 
 
 
-class Player:
-    def __init__(self, player) -> None:
-        self.player = player
+
+
+
+
+
+
+
 
